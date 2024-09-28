d5722d702d722d2e823b9bf97ee76ffb0f07e264
samyak <samyakchheda000@gmail.com>
2024-09-28 12:48:59 +0530

    added files

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json()); // To handle JSON requests
 
 // Connect to MongoDB Atlas
-mongoose.connect("mongodb+srv://samyakchheda000:xWsnDwwMEKzIZ6Kn@cluster0.4ajiu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
+mongoose.connect("mongodb+srv://samyakchheda000:xWsnDwwMEKzIZ6Kn@cluster0.4ajiu.mongodb.net/User", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => {
