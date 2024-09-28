a83167795d6b9d0a99d2416c777ef7f208ba534f
samyak <samyakchheda000@gmail.com>
2024-09-28 15:00:46 +0530

    modified code

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json()); // To handle JSON requests
 
 // Connect to MongoDB Atlas
-mongoose.connect("mongodb+srv://samyakchheda000:xWsnDwwMEKzIZ6Kn@cluster0.4ajiu.mongodb.net/user?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://samyakchheda000:xWsnDwwMEKzIZ6Kn@cluster0.4ajiu.mongodb.net/", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => {
